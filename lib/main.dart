56f80e5b2db7407298107aeb9c9c742af3c5576f
Avishka-Dulanjana <avishkadulanjana98@gmail.com>
2024-01-26 12:52:02 +0530

    Firebase Configuration done

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+import 'package:get/get.dart';
 
 import 'app.dart';
 
-void main() {
+Future<void> main() async {
+  // Todo: Initialize the Firebase
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform).then(
+    (FirebaseApp value) => Get.put(AuthenticationRepository()),
+  );
+
+  // Load all the Material Design / Theme / Localizations / Bindings
   runApp(const App());
 }
