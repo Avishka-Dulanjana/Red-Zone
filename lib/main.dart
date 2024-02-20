fb89f77042a2909afb34b25180c30a41b8ea9d37
Avishka-Dulanjana <avishkadulanjana98@gmail.com>
2024-02-21 03:45:58 +0530

    Need to correct

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:red_zone/features/disaster_main/controller/disaster_controller.dart';
+import 'package:red_zone/features/disaster_main/controller/disaster_fetch_controller.dart';
 import 'data/repositories/authentication/authentication_repository.dart';
-import 'features/disaster_main/controller/google_map_controller.dart';
 import 'firebase_options.dart';
 import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
@@ -28,6 +28,11 @@ Future<void> main() async {
   // Initialize the Firebase Disaster Controller
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform).then(
     (FirebaseApp value) => Get.put(DisasterController()),
+  );
+
+  // Initialize the Firebase Disaster fetch Controller
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform).then(
+    (FirebaseApp value) => Get.put(DisasterFetchController()),
   );
 
   // Load all the Material Design / Theme / Localizations / Bindings
