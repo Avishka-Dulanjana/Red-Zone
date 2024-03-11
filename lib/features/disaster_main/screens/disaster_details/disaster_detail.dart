ff997092dec79fb21be7630f782f6fd18e1786e2
Avishka-Dulanjana <avishkadulanjana98@gmail.com>
2024-03-11 14:01:50 +0530

    Google map screen and shimmer added

--- lib/features/disaster_main/screens/disaster_details/disaster_detail.dart
+++ lib/features/disaster_main/screens/disaster_details/disaster_detail.dart
@@ -6,6 +6,7 @@ import 'package:red_zone/common/widgets/custom_shapes/containers/section_heading
 import 'package:red_zone/features/disaster_main/models/disaster_model.dart';
 import 'package:red_zone/features/disaster_main/screens/disaster_details/widgets/disaster_details_widgets/disaster_detail_image_slider.dart';
 import 'package:red_zone/features/disaster_main/screens/disaster_details/widgets/disaster_details_widgets/disaster_details_contents.dart';
+import 'package:red_zone/features/disaster_main/screens/disaster_details/widgets/disaster_details_widgets/disaster_map.dart';
 import 'package:red_zone/features/disaster_main/screens/disaster_details/widgets/disaster_details_widgets/rating_share_widget.dart';
 import '../../../../utils/constants/sizes.dart';
 import '../../../product_review/screen/product_reviews.dart';
@@ -37,9 +38,16 @@ class DisasterDetails extends StatelessWidget {
 
             const Divider(),
             const SizedBox(height: TSizes.spaceBtwItems),
+
             // Disaster Details
             TDisasterDetails(disaster: disaster),
+            const SizedBox(height: TSizes.spaceBtwItems),
 
+            // fetch Disaster Map Container
+            TDisasterMap(
+              disaster: disaster,
+              isLoading: false,
+            ),
             const SizedBox(height: TSizes.spaceBtwItems),
 
             // Reviews
@@ -49,6 +57,7 @@ class DisasterDetails extends StatelessWidget {
               const TSectionHeading(title: '  Reviews(25)', showActionButton: false),
               IconButton(onPressed: () => Get.to(() => const ProductReviewsScreen()), icon: const Icon(Iconsax.arrow_right_3_copy, size: 18))
             ]),
+            const SizedBox(height: TSizes.spaceBtwItems),
           ],
         ),
       ),
